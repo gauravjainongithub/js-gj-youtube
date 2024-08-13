06df02ba59ec7216afc0166e2cc8482cbc5aed14
gauravjainongithub <gaurav_sanmacs@yahoo.co.in>
2024-08-13 18:19:19 +0000

    let, var and const ki kahani

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 112233;
+let accountEmail = "gjsir@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+accountEmail = "gjain@google.com";
+accountPassword = "22222";
+accountCity = "Bengaluru";
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
