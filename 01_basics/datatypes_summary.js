2db9a384dc6fa3d3ddfc6878711355ec2cbc1931
gauravjainongithub <gaurav_sanmacs@yahoo.co.in>
2024-08-30 04:32:17 +0000

    stack and heap difference and datatype summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -39,3 +39,30 @@ console.log(typeof obj);
 console.log(typeof fn);
 
 //https://262.ecma-international.org/
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+//stack(primitive), Heap(non-primitive)
+//stack is used for primitive datatypes and heap is used for non-primitive data types.
+//in stack, copy of the variable provided while in heap reference of the original value is provided.
+
+let value1 = "gj";
+let value2 = value1;
+value2 = "jain";
+console.log(value1);
+console.log(value2);
+
+let obj1 = {
+    email: "gj@visa.com",
+    upi: "gj@yml"
+}
+
+let obj2 = obj1;
+obj2.email = "gj@gmail.com";
+
+console.log(obj1.email);
+console.log(obj2.email);
+
+//stack is used by primitive data type and heap is used by non-primitive data type.when variable is cretaed on stack then only a copy of the variable is used at time of modification. 
+
+//Those are created on heap(non-primitive) they are accessed by their reference means any change u do will happen in actual place.
