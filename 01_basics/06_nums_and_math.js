48f9e815699e90cedda808e9210f89bf571ef0b3
gauravjainongithub <gaurav_sanmacs@yahoo.co.in>
2024-09-01 07:50:21 +0000

    Math and Num(change in line 36 )

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -33,6 +33,6 @@ console.log(Math.floor(Math.random()*10) + 1);
 let min = 10;
 let max = 20;
 
-console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+console.log(Math.floor(Math.random()*(max - min) + 1) + min);
 
 
