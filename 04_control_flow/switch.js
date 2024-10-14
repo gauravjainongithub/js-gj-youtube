dab14a8d61c13e76fe336e0cee6b567f5ea4b2e0
gauravjainongithub <gaurav_sanmacs@yahoo.co.in>
2024-10-14 05:55:20 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,71 @@
+/*
+switch(key){
+    case value:
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month = 3;
+
+switch(month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    
+    
+    default:
+        console.log("default");
+}
+
+/*const month = "mar";
+
+switch(month) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("march");
+        break;
+    case "apr":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    
+    
+    default:
+        console.log("default");
+}*/
+
+/*const val = false;
+switch(val){
+    case true:
+        console.log("true");
+        break;
+    case false:
+        console.log("false"); 
+        break;
+        default:
+            console.log("default");
+}*/
+
+
